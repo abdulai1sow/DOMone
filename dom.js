93132f0cd8759f612e79512d0af2a1637d03dc6c
sowabdulai <abdulai-as@hotmail.com>
2022-09-08 11:30:12 -0400

    first dom

---
+++ dom.js
@@ -0,0 +1,30 @@
+//grabing the title id
+const titleEl = document.getElementById('title')
+// change the element properties
+titleEl.style.backgroundColor = '#fff'
+titleEl.style.padding = '20px'
+titleEl.style.color = '#CFD2CF'
+titleEl.style.fontSize = '64px'
+console.log(titleEl);
+
+//queryselector to select an element without id/class 
+const h2 = document.querySelectorAll('h2')
+console.log(h2);
+
+
+
+
+
+
+
+
+
+
+
+// function addStyle(element) {
+//   element.style.backgroundColor = '#fff';
+//   element.style.padding = '20px';
+//   element.style.color = '#cfd2cf';
+//   element.style.fontSize = '64px'
+// }
+// addStyle(titleEl)
